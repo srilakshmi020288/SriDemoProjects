aef26defd20df469dd2cbbbe0f9f67f4cc1ea268
Vishwani <Vishwani@DESKTOP-NQTNSIK>
2024-03-15 13:43:02 +0530

    Third commit

---
+++ src/JavaProjects/Mul.java
@@ -0,0 +1,11 @@
+package JavaProjects;
+
+public class Mul {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=3; 
+		System.out.println(a*b);  
+
+	}
+
+}
---
+++ src/JavaProjects/Sub.java
@@ -0,0 +1,12 @@
+package JavaProjects;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=3; 
+		System.out.println(a-b);  
+
+	}
+
+}
